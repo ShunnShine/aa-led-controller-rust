af7d7bec88e76ad2f24bac43f5c8e830998319de
Jack Shunn <jack.shunn@gmail.com>
2023-06-19 14:57:15 -0600

    start connecting main with plan generator

--- src/main.rs
+++ src/main.rs
@@ -21,23 +21,28 @@ enum RequestJSON {
 #[post("/api/led_control")]
 async fn led_control(body: Json<RequestJSON>, send_channel: web::Data<mpsc::Sender<plan_generator::Message>>) -> Result<impl Responder> {
     let body = body.into_inner();
-    match body {
+    let try_handle_request = match body {
         RequestJSON::leds_on(leds) => {
-            println!("leds_on {:?}", leds);
+            send_channel.send(plan_generator::Message::LedsOn(leds)).err()
         }
         RequestJSON::leds_off(leds) => {
-            println!("leds_off {:?}", leds);
+            send_channel.send(plan_generator::Message::LedsOff(leds)).err()
         }
         RequestJSON::all_leds_on(state) => {
-            println!("all_leds_on {state}");
+            if state { send_channel.send(plan_generator::Message::AllLedsOn).err() } else { None }
         }
         RequestJSON::all_leds_off(state) => {
-            println!("all_leds_off {state}");
+            if state { send_channel.send(plan_generator::Message::AllLedsOff).err() } else { None }
         }
         RequestJSON::leds_blink(leds) => {
-            println!("leds_blink {:?}", leds);
+            send_channel.send(plan_generator::Message::LedsBlink(leds)).err()
         }
+    };
+    match try_handle_request {
+        Some(_e) => panic!("[Error] Could not send message to Plan Generator."),
+        _ => {}
     }
+
     Ok(Json(ResponseJSON { message : "Success!".to_string() }))
 }
 
--- src/plan_generator.rs
+++ src/plan_generator.rs
@@ -1,5 +1,6 @@
 use std::sync::mpsc::Receiver;
 
+
 pub enum Message {
     LedsOn(Vec<u8>),
     LedsOff(Vec<u8>),
@@ -8,6 +9,31 @@ pub enum Message {
     LedsBlink(Vec<u8>),
 }
 
-pub fn start(receiveChannel: Receiver<Message>) -> () {
+pub fn start(receive_channel: Receiver<Message>) -> () {
+    const NUMBER_OF_LEDS: u8 = 4 * 32;
+    let current_plan;
+    let new_plan;
 
+    
+
+    for message in receive_channel {
+        match message {
+            Message::LedsOn(leds) => {
+                
+            }
+            Message::LedsOff(leds) => {
+
+            }
+            Message::AllLedsOn => {
+
+            }
+            Message::AllLedsOff => {
+
+            }
+            Message::LedsBlink(leds) => {
+
+            }
+        }
+    }
+    panic!("[Error] Lost all sending channels in Plan Generator.")
 }
